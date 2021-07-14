42ec3e1c475f6d9cfa4e52eb6fbfbe38c61947bb
Mechanio <mechanio5522@gmail.com>
2021-07-14 11:41:19 +0300

    Some comments

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -3,13 +3,11 @@ from secrets import secret_key
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = secret_key
-
+app.config['SECRET_KEY'] = secret_key  # secret key in private file
 
 from routes import main
 
 app.register_blueprint(main)
-
 
 if __name__ == '__main__':
     app.run()
--- flaskProject/forms.py
+++ flaskProject/forms.py
@@ -2,5 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 
 
+# Stringfield for entering GitHub login
 class SearchForm(FlaskForm):
     login = StringField('Enter GitHub login')
